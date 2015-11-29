a88327c6f8c823b6ed5254a7409c0845898b726e
Navjot Singh <weavebytes@gmail.com>
2015-11-29 15:50:50 +0530

    added HTML provider

--- config/app.php
+++ config/app.php
@@ -145,6 +145,12 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * For enabling HTML Forms etc...
+         */
+        Collective\Html\HtmlServiceProvider::class,
+
+
     ],
 
     /*
@@ -192,6 +198,12 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+
+        /*
+         * For HTML/FORMS etc...
+         */
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
