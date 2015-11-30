4762ee88e53621d6f3c5a3c5e000fbfeaaf59cce
Navjot Singh <weavebytes@gmail.com>
2015-11-30 23:30:33 +0530

    Feature: list/delete customers [done]

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -37,7 +37,7 @@ class CustomerController extends Controller
      */
     public function create()
     {
-        //
+        return view('customer.create');
     }
 
     /**
@@ -54,10 +54,10 @@ class CustomerController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  Customer $customer
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Customer $customer)
     {
         //
     }
@@ -65,10 +65,10 @@ class CustomerController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  Customer $customer
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Customer $customer)
     {
         //
     }
@@ -77,10 +77,10 @@ class CustomerController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  Customer $customer
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Customer $customer)
     {
         //
     }
@@ -88,11 +88,15 @@ class CustomerController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  Customer $customer
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Customer $customer)
     {
-        //
+        $customer->delete();
+
+        Session::flash('flash_message', 'Customer deleted successfully!');
+
+        return Redirect::route('customer.index')->with('message', 'Customer deleted.');
     }
 }
--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -1,1 +1,58 @@
-customers
+@extends('master')
+ 
+@section('content')
+    <h3>My Customers</h3>
+
+    @if(Session::has('flash_message'))
+    <div class="alert alert-success">
+        <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+        {{ Session::get('flash_message') }}
+    </div>
+    @endif
+
+    @if ( !$customers->count() )
+        You have no customer
+    @else
+        <table class="table" border="0" width="100%">
+            <tr class="info"><td>Customer Name</td><td>Firm Name</td><td>Edit</td><td>Delete</td>
+            </tr>
+            @foreach( $customers as $customer )
+                <tr>
+
+                    {!! Form::open(array(
+                            'class' => 'form-inline', 
+                            'method' => 'DELETE', 
+                            'id' => 'frmDelete-' . $customer->id,
+                            'style' => 'display:inline',
+                            'route' => array('customer.destroy', $customer->id))) 
+                    !!}
+                        <td><a href="{{ route('customer.show', $customer->id) }}">{{ $customer->name }}</a></td>
+                        <td>{{ $customer->firm_name }}</td>
+                        
+                        <td>{!! link_to_route('customer.edit', 'Edit', array($customer), array('class' => 'btn btn-info')) !!}</td>
+                        <td>
+                            {!! 
+                                Form::button(
+                                'Delete', 
+                                 array('class'        => 'btn btn-danger', 
+                                       'data-toggle'  => 'modal',
+                                       'data-target'  => '#confirmDelete', 
+                                       'data-title'   => 'Delete customer',
+                                       'id'           => $customer->id ,
+                                       'data-message' => 'Are you sure you want to delete this customer ?'
+                                )) 
+                            !!}
+                        </td>
+                        
+                    {!! Form::close() !!}
+                </tr>
+            @endforeach
+        </ul>
+    @endif
+ 
+    <p>
+        {!! link_to_route('customer.create', 'Create Customer', '', array('class' => 'btn btn-info')) !!}
+    </p>
+@endsection
+ 
+
