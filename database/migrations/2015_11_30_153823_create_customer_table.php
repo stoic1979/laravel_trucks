0077b79e39a7c57ec5c755440f381a869f72798a
Navjot Singh <weavebytes@gmail.com>
2015-11-30 21:12:03 +0530

    added  migration for customer table

---
+++ database/migrations/2015_11_30_153823_create_customer_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->default(0);
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('name')->default(''); 
+            $table->string('firm_name')->default(''); 
+            $table->string('mobile_number')->default('');
+            $table->string('email')->default('');             
+            $table->longText('address')->default(''); 
+            $table->string('state')->default(''); 
+            $table->string('country')->default(''); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('customer');
+    }
+}
